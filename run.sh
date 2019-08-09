cea34e43dcb85555e53f981a51f86314a8ee7055
mrteera <inbox@mrteera.com>
2019-08-09 15:55:23 +0700

    Update training script

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export NGPUS=2
+export NGPUS=8
 # time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_R_50_C4_1x.yaml" --use-tensorboard
 # time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_R_50_C4_1x.yaml"
 # time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_R_50_FPN_1x.yaml"
 # time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_R_101_FPN_1x.yaml"
 # time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_X_101_32x8d_FPN_1x_rev2.yaml"
 # time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/dental/e2e_faster_rcnn_X_101_32x8d_FPN_1x.yaml"
-time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_X_101_32x8d_FPN_1x_v4.yaml"
+# time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_X_101_32x8d_FPN_1x_v4.yaml"
+time python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_fbnet_600.yaml"
 # time python tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_X_101_32x8d_FPN_1x_v4.yaml"
 
 # time python tools/train_net.py --config-file "configs/bridges/e2e_mask_rcnn_R_50_C4_1x.yaml" --use-tensorboard
